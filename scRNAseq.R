d132f42bad0003f7b22bdb8b49233a17d3a02ac6
Xi Wang <sunlightwang@gmail.com>
2017-04-02 19:50:59 +0200

    Update scRNAseq.R

--- scRNAseq.R
+++ scRNAseq.R
@@ -68,11 +68,11 @@ theme_graphOnly <- function(base_size=14, base_family="Helvetica") {
 
 
 scale_fill_Publication <- function(...){
-  discrete_scale("fill","Publication", manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  discrete_scale("fill","Publication", manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#cccc33","dd4444")), ...)
 }
 
 scale_colour_Publication <- function(...){
-  discrete_scale("colour","Publication", manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  discrete_scale("colour","Publication", manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#eeee33","dd4444")), ...)
 }
 
 scale_colour_Rainbow <- function(size = 10, ...){
