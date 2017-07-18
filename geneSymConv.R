424a316c6079bcda66963b5c1954f8c86e1bb8b7
Xi Wang <sunlightwang@gmail.com>
2017-07-18 14:23:13 +0200

    Update geneSymConv.R

--- geneSymConv.R
+++ geneSymConv.R
@@ -5,15 +5,15 @@ require("biomaRt")
 MouseGeneToHuman <- function(genelist){
   human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
   mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-  genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = genelist, 
+  genes = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = genelist, 
     mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=T)
-  return(genesV2)
+  return(genes)
 }
 
 HumanGeneToMouse <- function(genelist){
   human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
   mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
-  genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = genelist, 
+  genes = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = genelist, 
     mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
-  return(genesV2)
+  return(genes)
 }
