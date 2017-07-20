ad17491ca0ec8a4018193c2368618e1c22705ea9
Xi Wang <sunlightwang@gmail.com>
2017-07-20 20:48:47 +0200

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -25,7 +25,7 @@ heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1) {
   heatmap.2(as.matrix(data[idx,]), distfun=dist2, hclustfun=hclust2, col=bluered(64), sepwidth=c(0,0),
             reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean),
             scale="row", ColSideColors=cc, RowSideColors=cr[gr.row], key=TRUE, symkey=FALSE, labRow=FALSE,
-            rowsep=0, density.info="none", cexRow=1, cexCol=1, margins=c(6,11),  trace="none", srtCol=45)
+            rowsep=0, density.info="none", cexRow=1, cexCol=1, margins=c(10,10),  trace="none", srtCol=45)
 }
 # data <- matrix(rnorm(2000), ncol=10)
 # design.2 <- rep(1:2,5)
