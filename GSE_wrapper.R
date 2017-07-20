8ff3b08decc011c8de45e0e9b407dcb3d952ed22
Xi Wang <sunlightwang@gmail.com>
2017-07-20 23:26:20 +0200

    Update GSE_wrapper.R

--- GSE_wrapper.R
+++ GSE_wrapper.R
@@ -40,9 +40,29 @@ run_goseq <- function(DEgenelist, Allgenelist, genome=c("hg19", "mm10"), geneID=
 # pvals.2 <-  run_goseq(DEgenelist,  Allgenelist, "hg19", "geneSymbol", "GO:BP", FALSE)
 
 ### CAMERA
-run_camera <- function(expr_log2, design, genome=c("hg19", "mm10"), geneID=c("geneSymbol"),
+run_camera <- function(expr_log2, no.samples=c(2,2), genome=c("hg19", "mm10"), geneID=c("geneSymbol"),
                        test.cats=c("GO:CC", "GO:BP", "GO:MF", "KEGG"), inter_gene_cor=TRUE, 
                        minSize=10, maxSize=3000, gs_enrich_plot=T, topN=20) {
   
-  camera(expr_log2, list(set1=index1,set2=index2), design, inter.gene.cor=NA)
+  design <- cbind(Intercept=1,Group=c(rep(0,no.samples[1]), rep(1,no.samples[2])))
+  require(goseq)
+  reversemapping <- function(map) {
+    tmp=unlist(map,use.names=FALSE)
+    names(tmp)=rep(names(map),times=as.numeric(summary(map)[,1]))
+    return(split(names(tmp),as.vector(tmp)))
+  }
+  gene2cat <- getgo(rownames(expr_log2), genome, geneID, fetch.cats=test.cats)
+  cat2gene=reversemapping(gene2cat)
+  gene2cat=reversemapping(cat2gene)
+  
+  cat2genes.idx0 <- ids2indices(cat2gene, rownames(expr_log2), remove.empty=TRUE)
+  geneset.size <- lapply(cat2genes.idx0, length)
+  cat2genes.idx <- cat2genes.idx0[geneset.size >= minSize & geneset.size <= maxSize]
+
+  inter.gene.cor <- ifelse(inter_gene_cor, 0.01, 0)
+
+  camera.rst <- camera(expr_log2, cat2genes.idx, design, inter.gene.cor=inter.gene.cor)
+  camera.rst.topN <- camera.rst[1:topN, ]
+  camera.rst.topN.GS <- rownames(camera.rst.topN)
+  cat2genes.idx.topN <- cat2genes.idx[camera.rst.topN.GS]
 }
