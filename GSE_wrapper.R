a0d3febe147471f64d4e2a7dcb92a60cba8689c0
Xi Wang <sunlightwang@gmail.com>
2017-07-21 20:04:44 +0200

    Update GSE_wrapper.R

--- GSE_wrapper.R
+++ GSE_wrapper.R
@@ -84,9 +84,9 @@ run_camera <- function(expr_log2, sample.cat=c(1,1,2,2), genome=c("hg19", "mm10"
     df <- melt(sample.mean_expr)
     colnames(df) <- c("ID","type","expr")
                                                
-    p <- ggplot(df, aes(x=type, y=expr)) + geom_boxplot(aes(color=type), size=1) + geom_point() + 
-                 facet_wrap(~ ID, scales="free",nrow=plot.nrow,ncol=plot.ncol, labeller=label_wrap_gen(width=30)) + 
-                 theme_Publication() + theme(strip.text = element_text(face="plain", size=rel(0.5)))
+    p <- ggplot(df, aes(x=type, y=expr)) + geom_boxplot(aes(color=type), outlier.size=1) + geom_point() + 
+                 facet_wrap(~ ID, scales="free",nrow=plot.nrow,ncol=plot.ncol, labeller=label_wrap_gen(width=35)) + 
+                 theme_Publication() + theme(strip.text = element_text(face="plain", size=rel(0.3)))
     return(p)
     ## genes       
     #s1.idx <- design[,2] == unique(design[,2])[1]
