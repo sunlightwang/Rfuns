0f72aa04ea8a51b9a826c3828a4425fa99179aa1
Xi Wang <sunlightwang@gmail.com>
2017-07-21 20:21:47 +0200

    Update GSE_wrapper.R

--- GSE_wrapper.R
+++ GSE_wrapper.R
@@ -84,7 +84,7 @@ run_camera <- function(expr_log2, sample.cat=c(1,1,2,2), genome=c("hg19", "mm10"
     df <- melt(sample.mean_expr)
     colnames(df) <- c("ID","type","expr")
                                                
-    p <- ggplot(df, aes(x=type, y=expr)) + geom_boxplot(aes(color=type), size=0.3) + geom_point(size=1) + 
+    p <- ggplot(df, aes(x=type, y=expr)) + geom_boxplot(aes(color=type), size=0.3) + geom_point(size=0.5) + 
                  facet_wrap(~ ID, scales="free",nrow=plot.nrow,ncol=plot.ncol, labeller=label_wrap_gen(width=30)) + 
                  theme_Publication() + theme(strip.text = element_text(face="plain", size=rel(0.5)))
     return(p)
