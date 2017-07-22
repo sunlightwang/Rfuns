1113eb7ade56a668362c510b10075271de0c4c15
Xi Wang <sunlightwang@gmail.com>
2017-07-23 00:16:13 +0200

    Update dense_plot.R

--- dense_plot.R
+++ dense_plot.R
@@ -1,3 +1,5 @@
+source("https://raw.githubusercontent.com/sunlightwang/Rfuns/master/theme.R")
+
 dens_plot <- function(x, y, xlab="x", ylab="y") {
   df <- data.frame(x = x, y = y)
   ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) + scale_fill_gradientn(colours=c("blue","purple", "red"), name = "dens", trans = "log10") + 
