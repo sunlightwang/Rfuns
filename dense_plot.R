9b79e8fb9253c3ddef3bf0f100a3c2dbab7d8c84
Xi Wang <sunlightwang@gmail.com>
2017-07-23 00:18:54 +0200

    Update dense_plot.R

--- dense_plot.R
+++ dense_plot.R
@@ -5,3 +5,10 @@ dens_plot <- function(x, y, xlab="x", ylab="y") {
   ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) + scale_fill_gradientn(colours=c("blue","purple", "red"), name = "dens", trans = "log10") + 
   xlab(xlab) + ylab(ylab) + theme_Publication()
 }
+
+#require(cowplot)
+#  p1 <- dens_plot(log2(cpm[,1]+1), log2(cpm[,2]+1), colnames(cpm)[1], colnames(cpm)[2])
+#  p2 <- dens_plot(log2(cpm[,4]+1), log2(cpm[,5]+1), colnames(cpm)[4], colnames(cpm)[5])
+#  p3 <- dens_plot(log2(cpm[,1]+1), log2(cpm[,4]+1), colnames(cpm)[1], colnames(cpm)[4])
+#  p4 <- dens_plot(log2(cpm[,2]+1), log2(cpm[,5]+1), colnames(cpm)[2], colnames(cpm)[5])
+#  plot_grid(p1, p2, p3, p4, ncol=2, labels=c("A", "B", "C", "D"))
