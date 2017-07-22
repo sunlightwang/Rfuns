c4aad3fdda4a9cca010abe1fe957398f1bb8670b
Xi Wang <sunlightwang@gmail.com>
2017-07-23 00:24:25 +0200

    Update dense_plot.R

--- dense_plot.R
+++ dense_plot.R
@@ -12,3 +12,8 @@ dens_plot <- function(x, y, xlab="x", ylab="y") {
 #  p3 <- dens_plot(log2(cpm[,1]+1), log2(cpm[,4]+1), colnames(cpm)[1], colnames(cpm)[4])
 #  p4 <- dens_plot(log2(cpm[,2]+1), log2(cpm[,5]+1), colnames(cpm)[2], colnames(cpm)[5])
 #  plot_grid(p1, p2, p3, p4, ncol=2, labels=c("A", "B", "C", "D"))
+
+
+# ggplot(df, aes(x=x, y=y, color=class)) + geom_point(size=0.5) + ggtitle("title") + 
+#   geom_text_repel(aes(x=x, y=y, color=class, label=names), size=2, segment.size=0.1) + 
+#   theme_Publication() 
