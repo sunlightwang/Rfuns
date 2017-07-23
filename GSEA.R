b0c76ecbe095594dc00ff4c7f41524c107bfa4f3
Xi Wang <sunlightwang@gmail.com>
2017-07-24 00:55:17 +0200

    Update GSEA.R

--- GSEA.R
+++ GSEA.R
@@ -1,33 +1,21 @@
 ## this is modified from GSEA.R of Broad Institute
 
-GSEA.EnrichmentScore <- function(gene.list, gene.set, weighted.score.type = 1, correl.vector = NULL) {  
+GSEA.EnrichmentScore <- function(gene.score, gene.set, weighted.score.type = 1) {  
   #
-  # Computes the weighted GSEA score of gene.set in gene.list. 
+  # Computes the weighted GSEA score of gene.set with gene.score 
   # The weighted score type is the exponent of the correlation 
-  # weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted). When the score type is 1 or 2 it is 
-  # necessary to input the correlation vector with the values in the same order as in the gene list.
+  # weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted). 
   #
   # Inputs:
-  #   gene.list: The ordered gene list (e.g. integers indicating the original position in the input dataset)  
-  #   gene.set: A gene set (e.g. integers indicating the location of those genes in the input dataset) 
+  #   gene.score: gene scores associated with coditions. 
+  #   gene.set: list of gene sets
   #   weighted.score.type: Type of score: weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted)  
-  #  correl.vector: A vector with the coorelations (e.g. signal to noise scores) corresponding to the genes in the gene list 
   #
   # Outputs:
   #   ES: Enrichment score (real number between -1 and +1) 
   #   arg.ES: Location in gene.list where the peak running enrichment occurs (peak of the "mountain") 
   #   RES: Numerical vector containing the running enrichment score for all locations in the gene list 
   #   tag.indicator: Binary vector indicating the location of the gene sets (1's) in the gene list 
-  #
-  # The Broad Institute
-  # SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  # This software and its documentation are copyright 2003 by the
-  # Broad Institute/Massachusetts Institute of Technology.
-  # All rights are reserved.
-  #
-  # This software is supplied without any warranty or guaranteed support
-  # whatsoever. Neither the Broad Institute nor MIT can be responsible for
-  # its use, misuse, or functionality.
   
   tag.indicator <- sign(match(gene.list, gene.set, nomatch=0))    # notice that the sign is 0 (no tag) or 1 (tag) 
   no.tag.indicator <- 1 - tag.indicator 
