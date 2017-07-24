2d03a8d4822fa484a2599e1fa884fd820b8bf4d8
Xi Wang <sunlightwang@gmail.com>
2017-07-25 00:32:28 +0200

    Update GSEA.R

--- GSEA.R
+++ GSEA.R
@@ -1,21 +1,8 @@
-## this is modified from GSEA.R of Broad Institute
 
-GSEA.EnrichmentScore <- function(gene.score, gene.set, weighted.score.type = 1) {  
-  #
-  # Computes the weighted GSEA score of gene.set with gene.score 
-  # The weighted score type is the exponent of the correlation 
-  # weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted). 
-  #
-  # Inputs:
-  #   gene.score: gene scores associated with coditions. 
-  #   gene.set: list of gene sets
-  #   weighted.score.type: Type of score: weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted)  
-  #
-  # Outputs:
-  #   ES: Enrichment score (real number between -1 and +1) 
-  #   arg.ES: Location in gene.list where the peak running enrichment occurs (peak of the "mountain") 
-  #   RES: Numerical vector containing the running enrichment score for all locations in the gene list 
-  #   tag.indicator: Binary vector indicating the location of the gene sets (1's) in the gene list 
+
+GSEA.ES <- function(gene.score, gene.set, weighted.score.type = 1) {  
+  ngene <- length(gene.score)
+  nset <- length(gene.set)
   
   tag.indicator <- sign(match(gene.list, gene.set, nomatch=0))    # notice that the sign is 0 (no tag) or 1 (tag) 
   no.tag.indicator <- 1 - tag.indicator 
@@ -45,35 +32,8 @@ GSEA.EnrichmentScore <- function(gene.score, gene.set, weighted.score.type = 1) 
   return(list(ES = ES, arg.ES = arg.ES, RES = RES, indicator = tag.indicator))    
 }
 
-GSEA.EnrichmentScore2 <- function(gene.list, gene.set, weighted.score.type = 1, correl.vector = NULL) {  
-  #
-  # Computes the weighted GSEA score of gene.set in gene.list. It is the same calculation as in 
-  # GSEA.EnrichmentScore but faster (x8) without producing the RES, arg.RES and tag.indicator outputs.
-  # This call is intended to be used to asses the enrichment of random permutations rather than the 
-  # observed one.
-  # The weighted score type is the exponent of the correlation 
-  # weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted). When the score type is 1 or 2 it is 
-  # necessary to input the correlation vector with the values in the same order as in the gene list.
-  #
-  # Inputs:
-  #   gene.list: The ordered gene list (e.g. integers indicating the original position in the input dataset)  
-  #   gene.set: A gene set (e.g. integers indicating the location of those genes in the input dataset) 
-  #   weighted.score.type: Type of score: weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted)  
-  #  correl.vector: A vector with the coorelations (e.g. signal to noise scores) corresponding to the genes in the gene list 
-  #
-  # Outputs:
-  #   ES: Enrichment score (real number between -1 and +1) 
-  #
-  # The Broad Institute
-  # SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  # This software and its documentation are copyright 2003 by the
-  # Broad Institute/Massachusetts Institute of Technology.
-  # All rights are reserved.
-  #
-  # This software is supplied without any warranty or guaranteed support
-  # whatsoever. Neither the Broad Institute nor MIT can be responsible for
-  # its use, misuse, or functionality.
-  
+GSEA.ES2 <- function(gene.score, gene.set, weighted.score.type=1) {  
+
   N <- length(gene.list) 
   Nh <- length(gene.set) 
   Nm <-  N - Nh 
@@ -114,100 +74,12 @@ GSEA.EnrichmentScore2 <- function(gene.list, gene.set, weighted.score.type = 1, 
   max.ES <- max(peak.res.vector)
   min.ES <- min(valley.res.vector)
   ES <- signif(ifelse(max.ES > - min.ES, max.ES, min.ES), digits=5)
-  
   return(list(ES = ES))
-  
 }
-
-GSEA.EnrichmentScore2 <- function(gene.list, gene.set, weighted.score.type = 1, correl.vector = NULL) {  
-  #
-  # Computes the weighted GSEA score of gene.set in gene.list. It is the same calculation as in 
-  # GSEA.EnrichmentScore but faster (x8) without producing the RES, arg.RES and tag.indicator outputs.
-  # This call is intended to be used to asses the enrichment of random permutations rather than the 
-  # observed one.
-  # The weighted score type is the exponent of the correlation 
-  # weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted). When the score type is 1 or 2 it is 
-  # necessary to input the correlation vector with the values in the same order as in the gene list.
-  #
-  # Inputs:
-  #   gene.list: The ordered gene list (e.g. integers indicating the original position in the input dataset)  
-  #   gene.set: A gene set (e.g. integers indicating the location of those genes in the input dataset) 
-  #   weighted.score.type: Type of score: weight: 0 (unweighted = Kolmogorov-Smirnov), 1 (weighted), and 2 (over-weighted)  
-  #  correl.vector: A vector with the coorelations (e.g. signal to noise scores) corresponding to the genes in the gene list 
-  #
-  # Outputs:
-  #   ES: Enrichment score (real number between -1 and +1) 
-  #
-  # The Broad Institute
-  # SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  # This software and its documentation are copyright 2003 by the
-  # Broad Institute/Massachusetts Institute of Technology.
-  # All rights are reserved.
-  #
-  # This software is supplied without any warranty or guaranteed support
-  # whatsoever. Neither the Broad Institute nor MIT can be responsible for
-  # its use, misuse, or functionality.
-  
-  N <- length(gene.list) 
-  Nh <- length(gene.set) 
-  Nm <-  N - Nh 
-  
-  loc.vector <- vector(length=N, mode="numeric")
-  peak.res.vector <- vector(length=Nh, mode="numeric")
-  valley.res.vector <- vector(length=Nh, mode="numeric")
-  tag.correl.vector <- vector(length=Nh, mode="numeric")
-  tag.diff.vector <- vector(length=Nh, mode="numeric")
-  tag.loc.vector <- vector(length=Nh, mode="numeric")
-  
-  loc.vector[gene.list] <- seq(1, N)
-  tag.loc.vector <- loc.vector[gene.set]
-  
-  tag.loc.vector <- sort(tag.loc.vector, decreasing = F)
-  
-  if (weighted.score.type == 0) {
-    tag.correl.vector <- rep(1, Nh)
-  } else if (weighted.score.type == 1) {
-    tag.correl.vector <- correl.vector[tag.loc.vector]
-    tag.correl.vector <- abs(tag.correl.vector)
-  } else if (weighted.score.type == 2) {
-    tag.correl.vector <- correl.vector[tag.loc.vector]*correl.vector[tag.loc.vector]
-    tag.correl.vector <- abs(tag.correl.vector)
-  } else {
-    tag.correl.vector <- correl.vector[tag.loc.vector]**weighted.score.type
-    tag.correl.vector <- abs(tag.correl.vector)
-  }
-  
-  norm.tag <- 1.0/sum(tag.correl.vector)
-  tag.correl.vector <- tag.correl.vector * norm.tag
-  norm.no.tag <- 1.0/Nm
-  tag.diff.vector[1] <- (tag.loc.vector[1] - 1) 
-  tag.diff.vector[2:Nh] <- tag.loc.vector[2:Nh] - tag.loc.vector[1:(Nh - 1)] - 1
-  tag.diff.vector <- tag.diff.vector * norm.no.tag
-  peak.res.vector <- cumsum(tag.correl.vector - tag.diff.vector)
-  valley.res.vector <- peak.res.vector - tag.correl.vector
-  max.ES <- max(peak.res.vector)
-  min.ES <- min(valley.res.vector)
-  ES <- signif(ifelse(max.ES > - min.ES, max.ES, min.ES), digits=5)
-  
-  return(list(ES = ES))
-  
-}
-
 
 GSEA.HeatMapPlot <- function(V, row.names = F, col.labels, col.classes, col.names = F, main = " ", xlab=" ", ylab=" ") {
-  #
   # Plots a heatmap "pinkogram" of a gene expression matrix including phenotype vector and gene, sample and phenotype labels
-  #
-  # The Broad Institute
-  # SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  # This software and its documentation are copyright 2003 by the
-  # Broad Institute/Massachusetts Institute of Technology.
-  # All rights are reserved.
-  #
-  # This software is supplied without any warranty or guaranteed support
-  # whatsoever. Neither the Broad Institute nor MIT can be responsible for
-  # its use, misuse, or functionality.
-  
+ 
   n.rows <- length(V[,1])
   n.cols <- length(V[1,])
   row.mean <- apply(V, MARGIN=1, FUN=mean)
@@ -1250,5 +1122,4 @@ GSEA <- function(geneScores, geneSets, nperm = 1000, weighted.score.type = 1,
   
   return(list(report1 = report.phen1, report2 = report.phen2))
   
-}  # end of definition of GSEA.analysis
-
+} 
