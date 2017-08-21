a117feb20e06c588abcc524672b245539ba09a21
Xi Wang <sunlightwang@gmail.com>
2017-08-21 19:30:10 +0200

    Update GeneSet_Dm.R

--- GeneSet_Dm.R
+++ GeneSet_Dm.R
@@ -49,11 +49,12 @@ GS_enrich <- function(GeneList, bgGeneList=NULL, annots, padj_cutoff=0.05, minHi
   desc <- unique(data.frame(annots$GeneSet, annots$Desc))
   rownames(desc) <- desc$annots.GeneSet
   result <- data.frame(result, desc=as.character(noquote(desc[rownames(result),2])))
+  result.sig <- result[result$padj < padj_cutoff, ]
   if(plot) { 
-    p <- ggplot(result, aes(x=reorder(term, padj), y=padj)) + geom_bar(aes(fill=EnrichFold), stat="identity") +
+    p <- ggplot(result.sig, aes(x=reorder(term, padj), y=padj)) + geom_bar(aes(fill=EnrichFold), stat="identity") +
     coord_flip() + ylab("-log10(FDR)") + xlab("") + theme_Publication() + scale_fill_continuous(low="yellow", high="red")
     return(p)
   else { 
-    return(result[result$padj < padj_cutoff, ])
+    return(result.sig)
   }
 }               
