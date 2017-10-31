6216a6a398896c2426d7a25fa4e481830cf4fca0
Xi Wang <sunlightwang@gmail.com>
2017-10-31 10:23:48 +0100

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -4,7 +4,7 @@ hclust2 <- function(x, method="ward.D", ...)
 dist2 <- function(x, ...)
   as.dist(1-cor(t(x), method="spearman"))
 
-heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row",...) { 
+heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row",labRow=FALSE,cexRow=1, cexCol=1,margins=c(10,10),...) { 
 #n.row.class cannot be too large (<8)
   
   require(gplots)
@@ -24,8 +24,8 @@ heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row
   cr <- brewer.pal(n.row.class, "Set1")
   heatmap.2(as.matrix(data[idx,]), distfun=dist2, hclustfun=hclust2, col=bluered(64), sepwidth=c(0,0),
             reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean),
-            scale=scale, ColSideColors=cc, RowSideColors=cr[gr.row], key=TRUE, symkey=FALSE, labRow=FALSE,
-            rowsep=0, density.info="none", cexRow=1, cexCol=1, margins=c(10,10),  trace="none", srtCol=45,...)
+            scale=scale, ColSideColors=cc, RowSideColors=cr[gr.row], key=TRUE, symkey=FALSE, labRow=labRow,
+            rowsep=0, density.info="none", cexRow=cexRow, cexCol=cexCol, margins=margins,  trace="none", srtCol=45,...)
 }
 # data <- matrix(rnorm(2000), ncol=10)
 # design.2 <- rep(1:2,5)
