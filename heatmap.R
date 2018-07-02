443cbcc9fa9c68368b0e8051544a57a478c41bb3
Xi Wang <sunlightwang@gmail.com>
2018-07-02 22:04:18 +0200

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -4,7 +4,8 @@ hclust2 <- function(x, method="ward.D", ...)
 dist2 <- function(x, ...)
   as.dist(1-cor(t(x), method="spearman"))
 
-heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row",labRow=FALSE,cexRow=1, cexCol=1,margins=c(10,10),...) { 
+heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row",labRow=FALSE,cexRow=1, cexCol=1,margins=c(10,10),
+                            distfun=dist2, hclustfun=hclust2,...) { 
 #n.row.class cannot be too large (<8)
   
   require(gplots)
@@ -16,13 +17,13 @@ heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row
   idx <- apply(data, 1, var) > min.var
   
   ### perform clustering on rows
-  cl.row <- hclust2(dist2(data[idx,]))
+  cl.row <- hclustfun(distfun(data[idx,]))
   ### extract cluster assignments; i.e. k=4 (rows) 
   gr.row <- cutree(cl.row, n.row.class)
   ### require(RColorBrewer)
   require(RColorBrewer)
   cr <- brewer.pal(n.row.class, "Set1")
-  heatmap.2(as.matrix(data[idx,]), distfun=dist2, hclustfun=hclust2, col=bluered(64), sepwidth=c(0,0),
+  heatmap.2(as.matrix(data[idx,]), distfun=distfun, hclustfun=hclustfun, col=bluered(64), sepwidth=c(0,0),
             reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean),
             scale=scale, ColSideColors=cc, RowSideColors=cr[gr.row], key=TRUE, symkey=FALSE, labRow=labRow,
             rowsep=0, density.info="none", cexRow=cexRow, cexCol=cexCol, margins=margins,  trace="none", srtCol=45,...)
@@ -32,7 +33,8 @@ heatmap_cluster <- function(data, design.2, n.row.class=4, min.var=1, scale="row
 # heatmap_cluster(data, design.2)
 
 ###
-#  heatmap.2(cor(cpm.log2[gene.intersect,idx1], method="spearman"), dendrogram="both", distfun=dist, hclustfun=hclust2, col=colfunc(32), sepwidth=c(0,0),cexCol=1,cexRow=1,
+#  heatmap.2(cor(cpm.log2[gene.intersect,idx1], method="spearman"), dendrogram="both", distfun=distfun, hclustfun=hclustfun, 
+#            col=colfunc(32), sepwidth=c(0,0),cexCol=1,cexRow=1,
 #            reorderfun=function(d, w) reorder(d, w, agglo.FUN = mean), ColSideColors=cc[gr.row], symkey=F, srtCol=45,
 #            scale="none",density.info="none", trace="none", keysize=1, key.xlab="CC", key.title="", xlab="", margins=c(10,12))
 #
