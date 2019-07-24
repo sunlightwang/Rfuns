332b58eecfa5daf849ab658db4a70ad06c51895b
Xi Wang <sunlightwang@gmail.com>
2019-07-24 23:41:21 +0200

    Add files via upload

--- barcodeplot.R
+++ barcodeplot.R
@@ -17,10 +17,10 @@ barcodeplot <- function(data, sample1="rep1", sample2="rep2", colorby=NULL) {
   #data2 <- data0[apply(data0[,c("x","y")]>0, 1, all), c("x","y")]
   data2 <- data0[apply(data0[,c("x","y")]>0, 1, any), c("x","y")]
   ## both non-zero correlation 
-  r <- signif(cor(data2, method="spearman")[2,1],3)
+  r <- signif(cor(data2, method="spearman")[2,1],2)
   r.ci <- spearman.ci(data2[,1], data2[,2], nrep = 1000, conf.level = 0.95)
-  rlo <- signif(unname(r.ci$conf.int[1]),3)
-  rup <- signif(unname(r.ci$conf.int[2]),3)
+  rlo <- signif(unname(r.ci$conf.int[1]),2)
+  rup <- signif(unname(r.ci$conf.int[2]),2)
   ## one-zero range
   r1 <- range(data0[data0[,"y"]==0, "x"])
   r2 <- range(data0[data0[,"x"]==0, "y"])
@@ -28,8 +28,8 @@ barcodeplot <- function(data, sample1="rep1", sample2="rep2", colorby=NULL) {
   datapoly.x <- data.frame(x=c(r1[1]*0.8, r1[2]*1.25, r1[2]*1.25,  r1[1]*0.8), y=c(0.08,0.08,0.125,0.125))
   datapoly.y <- data.frame(y=c(r2[1]*0.8, r2[2]*1.25, r2[2]*1.25,  r2[1]*0.8), x=c(0.08,0.08,0.125,0.125))
   ## one-zero percentage
-  p.x <- signif(sum(data0[,"y"]==0)/nrow(data0) * 100, 3)
-  p.y <- signif(sum(data0[,"x"]==0)/nrow(data0) * 100, 3)
+  p.x <- signif(sum(data0[,"y"]==0)/nrow(data0) * 100, 2)
+  p.y <- signif(sum(data0[,"x"]==0)/nrow(data0) * 100, 2)
   data.text <- data.frame(x=c(0.1, r1[2]*1.3), y=c(r2[2]*1.3, 0.1), text=paste0(c(p.y,p.x),"%"))
   data.text <- rbind(data.text, data.frame(x=1,y=max(data1$y)*0.8,
                                            text=paste0("R=", r, " (", rlo, ",", rup, ")")))
