d8cf329b9c9864ad4c4e8d2bd55de789327d2d7b
Xi Wang <sunlightwang@gmail.com>
2019-07-27 19:29:01 +0200

    Add files via upload

--- Poisson_CI.R
+++ Poisson_CI.R
@@ -7,14 +7,14 @@ Poisson_CI <- function(N1=10000, N2=10000, CI=0.95, plot=T) {
   y <- 1:min(N1,N2)
   p1 <- x/N1
   p2 <- y/N2
-  delta_x <- sqrt((1 - p1) / x) * log10(exp(1))
-  delta_y <- sqrt((1 - p2) / y) * log10(exp(1))
+  delta_x <- sqrt((1 - p1) / x) * log10(exp(1)) * sqrt(2)
+  delta_y <- sqrt((1 - p2) / y) * log10(exp(1)) * sqrt(2)
   delta_M <- 2 * sqrt(delta_x ^ 2 * delta_y ^ 2 / (delta_x ^ 2 + delta_y ^ 2) ) 
   mu <- (x + y) / sqrt(2)
-  x_p1 <- log10(mu) / sqrt(2) - (ci_p * delta_M - log10(N1/N2)) / sqrt(2) 
-  y_p1 <- log10(mu) / sqrt(2) + (ci_p * delta_M - log10(N1/N2)) / sqrt(2) 
-  x_p2 <- log10(mu) / sqrt(2) - (-ci_p * delta_M - log10(N1/N2)) / sqrt(2) 
-  y_p2 <- log10(mu) / sqrt(2) + (-ci_p * delta_M - log10(N1/N2)) / sqrt(2) 
+  x_p1 <- log10(mu) / sqrt(2) - (ci_p * delta_M - log10(N1/N2) / sqrt(2)) / sqrt(2) 
+  y_p1 <- log10(mu) / sqrt(2) + (ci_p * delta_M - log10(N1/N2) / sqrt(2)) / sqrt(2)  
+  x_p2 <- log10(mu) / sqrt(2) - (-ci_p * delta_M - log10(N1/N2) / sqrt(2)) / sqrt(2) 
+  y_p2 <- log10(mu) / sqrt(2) + (-ci_p * delta_M - log10(N1/N2) / sqrt(2)) / sqrt(2) 
   if(plot) {
     plot(x_p1, y_p1, type="l", lty=2, xlim=c(-1,log10(N1)-0.5), ylim=c(-1,log10(N2)-0.5))
     lines(x_p2, y_p2, type="l", lty=2)
